9e9dfe7afd921df5caa36bdbc15a49e9d42059ad
NamHyeongKeol <grey_pastor@naver.com>
2016-06-30 02:30:24 +0900

    routes 수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'vote/index'
   get 'vote/result'
-  get 'vote/result/:t_id' => 'vote#result#:t_id'
-  get 'vote/secondvotecheck/:t_id'=> 'vote#secondvote#:t_id'
-  get 'vote/thirdvotecheck/:t_id'=> 'vote#thirdvote#:t_id'
+  get 'vote/result/:t_id' => 'vote#result'
+  get 'vote/secondvotecheck/:t_id'=> 'vote#secondvote'
+  get 'vote/thirdvotecheck/:t_id'=> 'vote#thirdvote'
   get 'vote/secondvote'
   get 'vote/thirdvote'
 
