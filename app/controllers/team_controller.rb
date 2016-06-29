224bf74b71b410308b9ce4de58c28c0901bab4d2
NamHyeongKeol <grey_pastor@naver.com>
2016-06-30 07:40:27 +0900

    team controller

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -22,21 +22,19 @@ class TeamController < ApplicationController
     o = Order.find(params[:order_id].to_i)
     c = o.card
     u = o.seller
-    flash[:success_notice] = "님아 돈이 없음"
+    flash[:success_notice] = "님아 돈이 없거나 님이 올린거 샀음"
 
-    if current_user.coin < o.price
+    if (current_user.coin < o.price) or (current_user.id != u.id)
       puts "zxcvzxcv"
       redirect_to "/team/buysell/#{t_id}"
     else
       Card.update(c.id, user_id: current_user.id)
       Order.update(o.id, buyer_id: current_user.id, is_complete: true)
 
-      if current_user.id != u.id
-        u.coin = u.coin + o.price
-        u.save
-        coin = current_user.coin
-        User.update(current_user.id, coin: coin - o.price)
-      end
+      u.coin = u.coin + o.price
+      u.save
+      coin = current_user.coin
+      User.update(current_user.id, coin: coin - o.price)
 
       flash[:success_notice] = "카드가 구매되었습니다"
       redirect_to "/team/buysell/#{t_id}"
@@ -48,21 +46,19 @@ class TeamController < ApplicationController
     c = current_user.cards.where(team_id: t_id).first
     o = Order.find(params[:order_id].to_i)
     u = o.buyer
-    flash[:success_notice] = "님아 이 팀에 대한 카드가 님한테 하나도 없음"
+    flash[:success_notice] = "님아 이 팀에 대한 카드가 님한테 하나도 없거나 님이 올린거 팔았음"
 
-    if c.nil?
+    if c.nil? or (current_user.id != u.id)
       puts "zxcvzxcv"
       redirect_to "/team/buysell/#{t_id}"
     else
       Card.update(c.id, user_id: u.id)
       Order.update(o.id, card_id: c.id, seller_id: current_user.id, buy_team_id: nil, is_complete: true)
 
-      if current_user.id != u.id
-        u.coin = u.coin - o.price
-        u.save
-        coin = current_user.coin
-        User.update(current_user.id, coin: coin + o.price)
-      end
+      u.coin = u.coin - o.price
+      u.save
+      coin = current_user.coin
+      User.update(current_user.id, coin: coin + o.price)
 
       flash[:success_notice] = "카드가 팔렸습니다"
       redirect_to "/team/buysell/#{t_id}"
